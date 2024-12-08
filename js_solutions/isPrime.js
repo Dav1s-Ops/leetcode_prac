38a1d51cabd959659e6c925c73492531bd087ae9
Davis Weimer <davisweimer@gmail.com>
2024-12-08 15:03:29 -0700

    Complete isPrime

---
+++ js_solutions/isPrime.js
@@ -0,0 +1,19 @@
+const isPrime = (n) => {
+  if (n <= 1) {
+    return false;
+  }
+  
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+console.log(isPrime(2)); // -> true
+console.log(isPrime(4)); // -> false
+console.log(isPrime(13)); // -> true
+console.log(isPrime(25)); // -> false
+console.log(isPrime(97)); // -> true
